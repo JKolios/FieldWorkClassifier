bfc5b5544063cef79d544fd545d3c75b180b0949
jkolios <jkolios@gophigital.com>
2017-04-11 00:34:52 +0300

    Project Cleanup

---
+++ PolygonImporter/vendor/github.com/JKolios/FieldWorkClassifier/Common/geojson/types.go
@@ -0,0 +1,47 @@
+package geojson
+
+/*Multipolygon is a representation of a GeoJSON Multipolygon,
+an array of distinct polygons.*/
+type Multipolygon struct {
+	Type string		       `json:"type"`
+	Coordinates [][][]Coordinate       `json:"coordinates"`
+}
+
+func NewMultipolygon(coordinates [][][]Coordinate) Multipolygon {
+	return Multipolygon{
+		Type:"multipolygon",
+		Coordinates:coordinates,
+	}
+}
+
+/*Polygon is a representation of a GeoJSON point*/
+type Point struct {
+	Type string		       `json:"type"`
+	Coordinates Coordinate       `json:"coordinates"`
+}
+
+func NewPoint(coordinates Coordinate) Point {
+	return Point{
+		Type:"point",
+		Coordinates:coordinates,
+	}
+}
+
+/*Coordinate is a pair of floats representing the latitude and longitude
+  of a single geographic point. In GeoJSON, these ar given as longitude,
+  latitude pairs*/
+
+type Coordinate [2]float64
+
+
+type FeatureCollection struct {
+	Features []Feature `json:"features"`
+}
+
+type Feature struct {
+	Geometry Geometry `json:"geometry"`
+}
+
+type Geometry struct {
+	Coordinates [][]Coordinate `json:"coordinates"`
+}
